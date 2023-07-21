f267f0cf7c8e9b8d3ee0c7f72f499eb6ca6c4390
Shaheenvp <113342233+Shaheenvp@users.noreply.github.com>
2023-07-21 10:19:58 +0530

    Add files via upload

---
+++ usertb.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 21, 2023 at 06:47 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usertb`
+--
+
+CREATE TABLE `usertb` (
+  `id` int(11) NOT NULL,
+  `action` varchar(20) NOT NULL,
+  `time` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `usertb`
+--
+
+INSERT INTO `usertb` (`id`, `action`, `time`) VALUES
+(23, 'edwdwlek', 'jdjd'),
+(24, 'ewdwevdsc', 'wecw');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `usertb`
+--
+ALTER TABLE `usertb`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `usertb`
+--
+ALTER TABLE `usertb`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
